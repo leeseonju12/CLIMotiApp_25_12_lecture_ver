9dad392d75e9193a346fa25b60152a9e57ba9862
leeseonju12 <seonju220@gmail.com>
2025-12-04 12:18:52 +0900

    종료, 등록, 목록 명령어 생성, 반복 구조

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+
+        new App(sc).run();
+
+        sc.close();
         }
     }
