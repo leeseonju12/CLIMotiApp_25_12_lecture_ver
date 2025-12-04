6036cb08664d3aeb5462de9ce88b3557890ddd8c
leeseonju12 <seonju220@gmail.com>
2025-12-04 17:07:33 +0900

    유지보수 및 협업을 위한 모듈 분리, 패키지 생성/이동, 리팩토링

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.entity.Motivation;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,12 +18,6 @@ public class App {
         System.out.println("== motivation 실행 ==");
 
         int lastId = 0;
-
-//        Motivation motivation0 = null;
-//        Motivation motivation1 = null;
-//        Motivation motivation2 = null;
-
-//        Motivation[] motivations = new Motivation[10];
 
         List<Motivation> motivations = new ArrayList<>();
 
@@ -44,62 +40,36 @@ public class App {
                 System.out.print("source : ");
                 String source = sc.nextLine();
 
-                Motivation motivation = new Motivation();
-                motivation.id = id;
-                motivation.body = body;
-                motivation.source = source;
+                Motivation motivation = new Motivation(id, body, source);
 
                 motivations.add(motivation);
-
-
-//                if (motivation.id == 1) {
-//                    motivation0 = motivation;
-//                } else if (motivation.id == 2) {
-//                    motivation1 = motivation;
-//                } else if (motivation.id == 3) {
-//                    motivation2 = motivation;
-//                }
-
-//                motivations[id - 1] = motivation;
 
                 System.out.printf("%d번 motivation이 등록되었습니다\n", id);
                 lastId++;
             } else if (cmd.equals("list")) {
                 System.out.println("=".repeat(40));
-                System.out.printf("   번호   /    source    /    motivation   \n");
-//                System.out.println(motivation2.toString());
-//                System.out.println(motivation1.toString());
-//                System.out.println(motivation0.toString());
-
-//                for (Motivation motivation : motivations) {
-//                    System.out.println(motivation.toString());
-//                }
+                System.out.printf("   번호   /    source      /    body   \n");
 
                 if (motivations.size() == 0) {
                     System.out.println("등록된거 없음 xxxxx");
-                } else {
-                    System.out.println("1개 이상 있음 oooo");
+                    continue;
                 }
+                for (int i = motivations.size() - 1; i >= 0; i--) {
+                    Motivation motivation = motivations.get(i);
 
+                    if (motivation.getSource().length() > 7) {
+                        System.out.printf("   %d     /    %s   /    %s   \n", motivation.getId(), motivation.getSource().substring(0, 7) + "...", motivation.getBody());
+                        continue;
+                    }
+                    System.out.printf("   %d     /    %s     /    %s   \n", motivation.getId(), motivation.getSource(), motivation.getBody());
+                }
+                System.out.println("=".repeat(40));
+
+            } else {
+                System.out.println("사용할 수 없는 명령어야");
             }
         }
 
 
     }
-}
-
-class Motivation {
-    int id;
-
-    @Override
-    public String toString() {
-        return "Motivation{" +
-                "id=" + id +
-                ", body='" + body + '\'' +
-                ", source='" + source + '\'' +
-                '}';
-    }
-
-    String body;
-    String source;
 }
